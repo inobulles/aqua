9b5f731727c76a3d7767feeea179c0bf93a14a2e
Aymeric Wibo <obiwac@gmail.com>
2025-01-04 18:08:14 +0100

    formatting: Bring in .clang-format

--- kos/kos.h
+++ kos/kos.h
@@ -5,8 +5,8 @@
 
 // Basic types.
 
-#include <stdint.h>
 #include <stdbool.h>
+#include <stdint.h>
 
 _Static_assert(sizeof(void*) <= sizeof(uint64_t), "pointers on your platform are too big");
 _Static_assert(sizeof(char) == sizeof(uint8_t), "char is not 8 bits");
@@ -24,9 +24,9 @@ typedef enum : uint64_t {
 } kos_api_vers_t;
 
 typedef struct {
-	kos_api_vers_t api_vers; // Current KOS API version. Should be set to `KOS_API_V4`.
+	kos_api_vers_t api_vers;      // Current KOS API version. Should be set to `KOS_API_V4`.
 	kos_api_vers_t best_api_vers; // Best API version theoretically supported by the KOS. Can be higher that `api_vers`.
-	uint8_t name[64]; // Something like "Unix-like system's KOS" or "ZED CPU FPGA KOS".
+	uint8_t name[64];             // Something like "Unix-like system's KOS" or "ZED CPU FPGA KOS".
 } kos_descr_v4_t;
 
 kos_api_vers_t kos_hello(kos_api_vers_t min, kos_api_vers_t max, kos_descr_v4_t* descr);
@@ -35,8 +35,8 @@ kos_api_vers_t kos_hello(kos_api_vers_t min, kos_api_vers_t max, kos_descr_v4_t*
 // Note that `kos_req_vdev` needs to be called after this one for the client to let the KOS know which VDEV specs it requires.
 
 typedef enum : uint8_t {
-	KOS_VDEV_KIND_GV, // Only available through the GrapeVine (because on another device e.g.), with explicit memory copies when memory needs to be actualized, i.e. slowest.
-	KOS_VDEV_KIND_UDS, // System-local to the app, communicating through UNIX domain sockets (or some other implementation-defined mechanism) and shared memory segments, i.e. very fast.
+	KOS_VDEV_KIND_GV,    // Only available through the GrapeVine (because on another device e.g.), with explicit memory copies when memory needs to be actualized, i.e. slowest.
+	KOS_VDEV_KIND_UDS,   // System-local to the app, communicating through UNIX domain sockets (or some other implementation-defined mechanism) and shared memory segments, i.e. very fast.
 	KOS_VDEV_KIND_LOCAL, // Memory-local to the app, i.e. potentially a little faster than `VDEV_KIND_UDS`, with less overhead.
 } kos_vdev_kind_t;
 
@@ -46,11 +46,11 @@ typedef struct __attribute__((packed)) {
 	uint64_t host_id; // A unique identifier for the host, could be MAC address of one of its interfaces e.g. All `VDEV_KIND_LOCAL` and `VDEV_KIND_UDS` VDEV's should have the same host ID a priori.
 	uint64_t vdev_id; // A unique identifier for the device within the host's namespace. Thus, a VDEV should be uniquely identifiable by just HID:VID.
 	uint8_t spec[64]; // Something like "aquabsd.black.wgpu".
-	uint32_t vers; // I don't know? Maybe an app could use this as grounds for a rejection before even discovering what commands the VDEV supports.
+	uint32_t vers;    // I don't know? Maybe an app could use this as grounds for a rejection before even discovering what commands the VDEV supports.
 	uint8_t human[256];
 	uint8_t vdriver_human[256]; // Something like "Default .wgpu device for aquaBSD Black".
-	kos_vdev_kind_t kind; // If the WGPU VDEV was on another machine, `VDEV_KIND_GV`.
-	uint32_t pref; // The KOS' preference level for this device. Could just be 0, or something higher to indicate the KOS would rather the app use this one than another.
+	kos_vdev_kind_t kind;       // If the WGPU VDEV was on another machine, `VDEV_KIND_GV`.
+	uint32_t pref;              // The KOS' preference level for this device. Could just be 0, or something higher to indicate the KOS would rather the app use this one than another.
 } kos_vdev_descr_t;
 
 typedef enum {
