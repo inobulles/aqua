3466428388026071a8767f8ef057401b781858a7
Aymeric Wibo <obiwac@gmail.com>
2025-01-18 13:05:53 +0100

    gv: Documentation for `gv_query_vdevs`

--- gv/gv.h
+++ gv/gv.h
@@ -11,4 +11,12 @@
 #define GV_NODES_PATH "/tmp/gv.nodes"
 #define GV_LOCK_PATH "/tmp/gv.lock"
 
+/**
+ * Get all the VDEVs found by the GrapeVine daemon.
+ *
+ * This function reads the GrapeVine nodes file which should've been populated by the daemon for node entries and their reported VDEVs.
+ *
+ * @param vdevs_out The VDEVs found by the daemon.
+ * @return The number of VDEVs found, 0 if the daemon is not running, and -1 if some unexpected error occurred.
+ */
 ssize_t gv_query_vdevs(kos_vdev_descr_t** vdevs_out);
