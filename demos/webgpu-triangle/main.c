726f5429f2ae14427ce49af501f2c5e202c62779
Aymeric Wibo <obiwac@gmail.com>
2025-04-11 14:50:11 -0700

    demos/webgpu-triangle: Use new library components thing

--- demos/webgpu-triangle/main.c
+++ demos/webgpu-triangle/main.c
@@ -1,80 +1,60 @@
 // This Source Form is subject to the terms of the AQUA Software License,
 // v. 1.0. Copyright (c) 2025 Aymeric Wibo
 
-#include <aqua/kos.h>
-#include <aqua/win.h>
+// #include <aqua/aqua.h>
+// #include <aqua/win.h>
+
+#include "../../lib/aqua.h"
+#include "../../lib/win.h"
 
 #include <assert.h>
 #include <inttypes.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct {
-	win_softc_t win_sc;
-	win_t win;
-} state_t;
-
-static void vdev_notif_cb(kos_notif_t const* notif, void* data) {
-	state_t* const state = data;
-
-	switch (notif->kind) {
-	case KOS_NOTIF_ATTACH:;
-		kos_vdev_descr_t const* const vdev = &notif->attach.vdev;
-
-		if (win_probe(vdev) && state->win_sc == NULL) {
-			// Connect to the first window VDEV we find.
-
-			printf("Found window VDEV: %s\n", vdev->human);
-			state->win_sc = win_conn(vdev);
-			kos_flush(true);
-		}
-
-		break;
-	case KOS_NOTIF_DETACH:
-		printf("VDEV detached: %" PRIu64 ":%" PRIu64 "\n", notif->detach.host_id, notif->detach.vdev_id);
-		break;
-	case KOS_NOTIF_CONN_FAIL:
-		fprintf(stderr, "Connection to VDEV failed\n");
-		break;
-	case KOS_NOTIF_CONN:
-		win_notif_conn(state->win_sc, notif);
-		break;
-	case KOS_NOTIF_CALL_FAIL:
-		fprintf(stderr, "Failed to call function on window VDEV\n");
-		break;
-	case KOS_NOTIF_CALL_RET:
-		win_notif_call_ret(state->win_sc, notif);
-		break;
-	case KOS_NOTIF_INTERRUPT:
-		win_interrupt(state->win, notif);
-		break;
-	}
-}
-
 int main(void) {
-	// Initiate communication with the KOS.
+	aqua_ctx_t const ctx = aqua_init();
 
-	kos_descr_v4_t descr;
-
-	if (kos_hello(KOS_API_V4, KOS_API_V4, &descr) != KOS_API_V4) {
+	if (ctx == NULL) {
+		fprintf(stderr, "Failed to initialize AQUA library\n");
 		return EXIT_FAILURE;
 	}
 
-	printf("Loaded KOS v4: %s\n", descr.name);
+	printf("KOS name: %s\n", aqua_get_kos_descr(ctx)->name);
 
-	state_t state = {
-		.win_sc = NULL,
-	};
+	// Setup window library component.
 
-	kos_sub_to_notif(vdev_notif_cb, &state);
+	aqua_component_t const win_comp = win_init(ctx);
 
-	// Setup window.
+	// Get the first window VDEV and create library component context from that.
+	// TODO Maybe I should have a simple function to do this for me automatically? I.e. init + find best VDEV + connection. Maybe even roll in aqua_init somehow.
 
-	win_init();
+	kos_vdev_descr_t* vdev = NULL;
 
-	state.win = win_create(state.win_sc);
-	win_loop(state.win);
-	win_destroy(state.win);
+	for (aqua_vdev_it_t it = aqua_vdev_it(win_comp); it.vdev != NULL; aqua_vdev_it_next(&it)) {
+		vdev = it.vdev;
+		break;
+	}
+
+	if (vdev == NULL) {
+		fprintf(stderr, "No window VDEV found\n");
+		return EXIT_FAILURE;
+	}
+
+	printf("Using window VDEV %s\n", (char*) vdev->human);
+	win_ctx_t const win_ctx = win_conn(vdev);
+
+	// Create window and loop it.
+
+	win_t const win = win_create(win_ctx);
+
+	if (win == NULL) {
+		fprintf(stderr, "Failed to create window\n");
+		return EXIT_FAILURE;
+	}
+
+	win_loop(win);
+	win_destroy(win);
 
 	return EXIT_SUCCESS;
 }
