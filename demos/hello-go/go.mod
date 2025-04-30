1ef07629fd140c7dfdc32d351784f1a02fe64a97
Aymeric Wibo <obiwac@gmail.com>
2025-04-30 21:30:56 +0200

    demos/hello-go: Go demo

---
+++ demos/hello-go/go.mod
@@ -0,0 +1,6 @@
+module hello-go
+
+go 1.24.0
+
+require obiw.ac/aqua v0.0.0
+replace obiw.ac/aqua => ../../lib/bindings/go
---
+++ demos/hello-go/main.go
@@ -0,0 +1,20 @@
+// This Source Form is subject to the terms of the AQUA Software License, v. 1.0.
+// Copyright (c) 2025 Aymeric Wibo
+
+package main
+
+import "fmt"
+import "obiw.ac/aqua"
+
+func main() {
+	ctx := aqua.Init()
+
+	if ctx == nil {
+		fmt.Println("Failed to initialize AQUA context")
+		return
+	}
+
+	descr := ctx.GetKosDescr()
+
+	fmt.Printf("AQUA context initialized successfully: KOS v%d, %s\n", descr.Api_vers, descr.Name)
+}
