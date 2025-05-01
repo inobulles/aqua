41501ad4916640398eea44b6c402f79685f457e1
Aymeric Wibo <obiwac@gmail.com>
2025-05-01 20:07:14 +0200

    lib/bindings/go: UI lib bindings

---
+++ lib/bindings/go/ui.go
@@ -0,0 +1,21 @@
+// This Source Form is subject to the terms of the AQUA Software License, v. 1.0.
+// Copyright (c) 2025 Aymeric Wibo
+
+package aqua
+
+/*
+#cgo LDFLAGS: -laqua_ui
+
+#include <aqua/ui.h>
+*/
+import "C"
+
+func (c *Context) UiInit() *Component {
+	comp := C.ui_init(c.ctx)
+
+	if comp == nil {
+		return nil
+	}
+
+	return &Component{comp: comp}
+}
