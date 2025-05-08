70aa46bb9ed8770591edcdfec44405c5b6657cba
Aymeric Wibo <obiwac@gmail.com>
2025-05-08 23:33:21 +0200

    lib/ui: Pull out internal struct defs into `ui_internal.h`

--- lib/ui.c
+++ lib/ui.c
@@ -6,6 +6,8 @@
 #define __AQUA_LIB_COMPONENT__
 #include "component.h"
 
+#include "ui_internal.h"
+
 #include <assert.h>
 #include <string.h>
 
@@ -13,47 +15,6 @@
 #define UMBER_COMPONENT "aqua.lib.ui"
 
 #define SPEC "aquabsd.black.ui"
-
-typedef struct {
-	uint32_t init;
-	uint32_t render;
-} backend_fns_t;
-
-struct ui_ctx_t {
-	// TODO This is pretty repetitive between library components - maybe we should inherit from a base struct?
-
-	uint64_t hid;
-	uint64_t vid;
-
-	kos_cookie_t last_cookie;
-	kos_val_t last_ret;
-	bool last_success;
-
-	bool is_conn;
-	uint64_t conn_id;
-
-	struct {
-		uint32_t ELEM_KIND_DIV;
-		uint32_t ELEM_KIND_TEXT;
-	} consts;
-
-	struct {
-		uint32_t create;
-		uint32_t destroy;
-		uint32_t get_root;
-		uint32_t add;
-	} fns;
-
-	// Backend-specific functions.
-
-	ui_supported_backends_t supported_backends;
-	backend_fns_t backend_wgpu_fns;
-};
-
-struct ui_t {
-	ui_ctx_t ctx;
-	void* opaque_ptr;
-};
 
 static component_t comp;
 
---
+++ lib/ui_internal.h
@@ -0,0 +1,51 @@
+// This Source Form is subject to the terms of the AQUA Software License,
+// v. 1.0. Copyright (c) 2025 Aymeric Wibo
+
+#pragma once
+
+#if !defined(__AQUA_LIB_COMPONENT__)
+# error "This file should only be included by AQUA library component source files."
+#endif
+
+#include "ui.h"
+
+typedef struct {
+	uint32_t init;
+	uint32_t render;
+} backend_fns_t;
+
+struct ui_ctx_t {
+	// TODO This is pretty repetitive between library components - maybe we should inherit from a base struct?
+
+	uint64_t hid;
+	uint64_t vid;
+
+	kos_cookie_t last_cookie;
+	kos_val_t last_ret;
+	bool last_success;
+
+	bool is_conn;
+	uint64_t conn_id;
+
+	struct {
+		uint32_t ELEM_KIND_DIV;
+		uint32_t ELEM_KIND_TEXT;
+	} consts;
+
+	struct {
+		uint32_t create;
+		uint32_t destroy;
+		uint32_t get_root;
+		uint32_t add;
+	} fns;
+
+	// Backend-specific functions.
+
+	ui_supported_backends_t supported_backends;
+	backend_fns_t backend_wgpu_fns;
+};
+
+struct ui_t {
+	ui_ctx_t ctx;
+	void* opaque_ptr;
+};
