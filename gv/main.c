ff89d38bfc142c706dc918f4dc795e626459519e
Aymeric Wibo <obiwac@gmail.com>
2025-08-07 22:28:10 +0200

    gv: Put VDEV inventory code in vdriver_loader

--- gv/main.c
+++ gv/main.c
@@ -1,12 +1,12 @@
-// This Source Form is subject to the terms of the AQUA Software License,
-// v. 1.0. Copyright (c) 2024 Aymeric Wibo
+// This Source Form is subject to the terms of the AQUA Software License, v. 1.0.
+// Copyright (c) 2024-2025 Aymeric Wibo
 
 #include "conn.h"
 #include "elp.h"
 #include "gv.h"
-#include "vdev.h"
 
 #include <aqua/gv_ipc.h>
+#include <aqua/vdriver_loader.h>
 
 #include <assert.h>
 #include <errno.h>
@@ -24,15 +24,32 @@
 #include <sys/file.h>
 #include <sys/socket.h>
 
+static void vdev_inventory_notif_cb(kos_notif_t const* notif, void* data) {
+	state_t* const state = data;
+
+	switch (notif->kind) {
+	case KOS_NOTIF_ATTACH:
+		state->vdevs = realloc(state->vdevs, ++state->vdev_count * sizeof *state->vdevs);
+		assert(state->vdevs != NULL);
+		state->vdevs[state->vdev_count - 1] = notif->attach.vdev;
+
+		LOG_I(state->init_cls, "Attached VDEV: %s", notif->attach.vdev.human);
+
+		break;
+	default:
+		break;
+	}
+}
+
 int main(int argc, char* argv[]) {
 	int rv = EXIT_FAILURE;
 	state_t state;
 
-	umber_class_t const* const init_cls = umber_class_new("aqua.gvd.init", UMBER_LVL_VERBOSE, "GrapeVine daemon initialization.");
-	umber_class_t const* const conn_listener = umber_class_new("aqua.gvd.listener", UMBER_LVL_VERBOSE, "GrapeVine daemon connection listener.");
+	state.init_cls = umber_class_new("aqua.gvd.init", UMBER_LVL_VERBOSE, "GrapeVine daemon initialization.");
+	state.listener_cls = umber_class_new("aqua.gvd.listener", UMBER_LVL_VERBOSE, "GrapeVine daemon connection listener.");
 	state.elp_cls = umber_class_new("aqua.gvd.elp", UMBER_LVL_INFO, "GrapeVine daemon echolocation (ELP) subsystem.");
 
-	LOG_V(init_cls, "Parsing options.");
+	LOG_V(state.init_cls, "Parsing options.");
 
 	char* interface_name = NULL;
 
@@ -41,11 +58,11 @@ int main(int argc, char* argv[]) {
 	while ((c = getopt(argc, argv, "i:")) != -1) {
 		switch (c) {
 		case 'i':
-			LOG_V(init_cls, "Setting interface name to %s.", optarg);
+			LOG_V(state.init_cls, "Setting interface name to %s.", optarg);
 			interface_name = optarg;
 			break;
 		default:
-			LOG_F(init_cls, "Unknown option: %c", c);
+			LOG_F(state.init_cls, "Unknown option: %c", c);
 			goto err_getopt;
 		}
 	}
@@ -54,33 +71,33 @@ int main(int argc, char* argv[]) {
 	argv += optind;
 
 	if (argc != 0) {
-		LOG_F(init_cls, "More arguments than expected.");
+		LOG_F(state.init_cls, "More arguments than expected.");
 		goto err_getopt;
 	}
 
 	if (interface_name == NULL) {
-		LOG_F(init_cls, "Interface name (-i) is required.");
+		LOG_F(state.init_cls, "Interface name (-i) is required.");
 		goto err_getopt;
 	}
 
-	LOG_V(init_cls, "Acquiring lock file %s.", GV_LOCK_PATH);
+	LOG_V(state.init_cls, "Acquiring lock file %s.", GV_LOCK_PATH);
 
 	// See https://0pointer.de/blog/projects/locking.html
 
 	FILE* const lock_file = fopen(GV_LOCK_PATH, "w");
 
 	if (lock_file == NULL) {
-		LOG_F(init_cls, "fopen: %s", strerror(errno));
+		LOG_F(state.init_cls, "fopen: %s", strerror(errno));
 		goto err_lock;
 	}
 
 	if (flock(fileno(lock_file), LOCK_EX | LOCK_NB) < 0) {
 		if (errno == EWOULDBLOCK) {
-			LOG_F(init_cls, "Another instance of the GrapeVine daemon is already running.");
+			LOG_F(state.init_cls, "Another instance of the GrapeVine daemon is already running.");
 		}
 
 		else {
-			LOG_F(init_cls, "flock: %s", strerror(errno));
+			LOG_F(state.init_cls, "flock: %s", strerror(errno));
 		}
 
 		goto err_lock;
@@ -88,23 +105,21 @@ int main(int argc, char* argv[]) {
 
 	fprintf(lock_file, "%d\n", getpid());
 
-	LOG_V(init_cls, "Making an inventory of local VDEVs we can make available.");
+	LOG_V(state.init_cls, "Making an inventory of local VDEVs we can make available.");
 
 	state.vdev_count = 0;
 	state.vdevs = NULL;
 
-	if (vdev_inventory(&state) < 0) {
-		LOG_F(init_cls, "vdev_inventory failed.");
-		goto err_vdev_inventory;
-	}
+	vdriver_loader_init();
+	vdriver_loader_vdev_local_inventory(vdev_inventory_notif_cb, &state);
 
-	LOG_I(init_cls, "Found and inventoried %zu VDEVs.", state.vdev_count);
-	LOG_V(init_cls, "Find the interface's IPv4 address.");
+	LOG_I(state.init_cls, "Found and inventoried %zu VDEVs.", state.vdev_count);
+	LOG_V(state.init_cls, "Find the interface's IPv4 address.");
 
 	struct ifaddrs* ifap;
 
 	if (getifaddrs(&ifap) < 0) {
-		LOG_F(init_cls, "getifaddrs: %s", strerror(errno));
+		LOG_F(state.init_cls, "getifaddrs: %s", strerror(errno));
 		goto err_getifaddrs;
 	}
 
@@ -132,7 +147,7 @@ int main(int argc, char* argv[]) {
 			state.found_ipv4 = ifa;
 			break;
 		case AF_INET6:
-			LOG_W(init_cls, "Interface %s has an IPv6 address, but GrapeVine only supports IPv4 for now (I know, shame, shame). Skipping.", interface_name);
+			LOG_W(state.init_cls, "Interface %s has an IPv6 address, but GrapeVine only supports IPv4 for now (I know, shame, shame). Skipping.", interface_name);
 			break;
 		default:
 			break;
@@ -140,23 +155,23 @@ int main(int argc, char* argv[]) {
 	}
 
 	if (state.found_ipv4 == NULL || state.found_ether == NULL) {
-		LOG_F(init_cls, iface_found ? "Interface %s does not have required addresses.\n" : "Interface %s not found.\n", interface_name);
+		LOG_F(state.init_cls, iface_found ? "Interface %s does not have required addresses.\n" : "Interface %s not found.\n", interface_name);
 		goto err_no_iface;
 	}
 
 	LOG_I(
-		init_cls,
+		state.init_cls,
 		"Found interface %s with IPv4 address %s.",
 		interface_name,
 		inet_ntoa(((struct sockaddr_in*) state.found_ipv4->ifa_addr)->sin_addr)
 	);
 
-	LOG_V(init_cls, "Creating socket for TCP connections (binding to port 0x%x).", GV_PORT);
+	LOG_V(state.init_cls, "Creating socket for TCP connections (binding to port 0x%x).", GV_PORT);
 
 	state.sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 
 	if (state.sock < 0) {
-		LOG_F(init_cls, "socket: %s", strerror(errno));
+		LOG_F(state.init_cls, "socket: %s", strerror(errno));
 		goto err_socket;
 	}
 
@@ -169,27 +184,27 @@ int main(int argc, char* argv[]) {
 	};
 
 	if (bind(state.sock, (struct sockaddr*) &addr, sizeof addr) < 0) {
-		LOG_F(init_cls, "bind: %s", strerror(errno));
+		LOG_F(state.init_cls, "bind: %s", strerror(errno));
 		goto err_bind;
 	}
 
-	LOG_V(init_cls, "Start the echolocation (ELP) subsystem.");
+	LOG_V(state.init_cls, "Start the echolocation (ELP) subsystem.");
 
 	if (elp(&state) < 0) {
-		LOG_F(init_cls, "Starting echolocation (ELP) subsystem failed.");
+		LOG_F(state.init_cls, "Starting echolocation (ELP) subsystem failed.");
 		goto err_elp;
 	}
 
-	LOG_V(init_cls, "Starting to listen for connections.");
+	LOG_V(state.init_cls, "Starting to listen for connections.");
 
 	if (listen(state.sock, 5) < 0) { // TODO What's a sane default for the backlog number? Should this be configurable?
-		LOG_F(init_cls, "listen: %s", strerror(errno));
+		LOG_F(state.init_cls, "listen: %s", strerror(errno));
 		goto err_listen;
 	}
 
 	// Wait for connections.
 
-	LOG_I(init_cls, "GrapeVine daemon bound to port 0x%x and listening for connections.", GV_PORT);
+	LOG_I(state.init_cls, "GrapeVine daemon bound to port 0x%x and listening for connections.", GV_PORT);
 
 	state.connection_count = 0;
 	state.connections = NULL;
@@ -197,25 +212,25 @@ int main(int argc, char* argv[]) {
 	for (;;) {
 		conn_t conn;
 
-		LOG_V(conn_listener, "Waiting for new connection.");
+		LOG_V(state.listener_cls, "Waiting for new connection.");
 
 		conn.state = &state;
 		conn.sock = accept(state.sock, (struct sockaddr*) &conn.addr, &conn.addr_len);
 
 		if (conn.sock < 0) {
-			LOG_F(conn_listener, "accept: %s", strerror(errno));
+			LOG_F(state.listener_cls, "accept: %s", strerror(errno));
 			goto err_accept;
 		}
 
 		LOG_I(
-			conn_listener,
+			state.listener_cls,
 			"Accepted connection from %s:0x%x (host %" PRIx64 ").",
 			inet_ntoa(conn.addr.sin_addr),
 			ntohs(conn.addr.sin_port),
 			sockaddr_to_mac((struct sockaddr*) &conn.addr)
 		);
 
-		LOG_V(conn_listener, "Finding/creating slot for connection.");
+		LOG_V(state.listener_cls, "Finding/creating slot for connection.");
 
 		conn_t* slot = NULL;
 
@@ -236,17 +251,17 @@ int main(int argc, char* argv[]) {
 		*slot = conn;
 		slot->slot_used = true;
 
-		LOG_V(conn_listener, "Starting connection thread.");
+		LOG_V(state.listener_cls, "Starting connection thread.");
 
 		pthread_create(&slot->thread, NULL, conn_thread, slot);
 	}
 
 	rv = EXIT_SUCCESS;
-	LOG_I(init_cls, "GrapeVine daemon is shutting down gracefully.");
+	LOG_I(state.init_cls, "GrapeVine daemon is shutting down gracefully.");
 
 err_accept:
 
-	LOG_V(init_cls, "Joining all connection threads.");
+	LOG_V(state.init_cls, "Joining all connection threads.");
 
 	if (state.connections != NULL) {
 		for (size_t i = 0; i < state.connection_count; i++) {
@@ -280,9 +295,7 @@ err_getifaddrs:
 		free(state.vdevs);
 	}
 
-err_vdev_inventory:
-
-	LOG_V(init_cls, "Releasing lock file %s.", GV_LOCK_PATH);
+	LOG_V(state.init_cls, "Releasing lock file %s.", GV_LOCK_PATH);
 
 	flock(fileno(lock_file), LOCK_UN); // XXX Shouldn't be necessary, but just in case.
 	fclose(lock_file);
@@ -290,7 +303,7 @@ err_vdev_inventory:
 err_lock:
 err_getopt:
 
-	LOG_V(init_cls, "GrapeVine daemon finished with status %d.", rv);
+	LOG_V(state.init_cls, "GrapeVine daemon finished with status %d.", rv);
 
 	return rv;
 }
