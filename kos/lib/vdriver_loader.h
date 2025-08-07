3294559d04504ced874b213b47d8a2f78ee0f430
Aymeric Wibo <obiwac@gmail.com>
2025-08-07 12:27:08 +0200

    kos: Start work on VDRIVER loader library

---
+++ kos/lib/vdriver_loader.h
@@ -0,0 +1,66 @@
+// This Source Form is subject to the terms of the AQUA Software License,
+// v. 1.0. Copyright (c) 2025 Aymeric Wibo
+
+/**
+ * Interface for the VDRIVER loader.
+ *
+ * This is used by the KOS to request VDEV specs and load relevant VDRIVERs.
+ * It is also used by the GrapeVine daemon to take inventory of all VDEVs available on the system.
+ */
+
+#pragma once
+
+#include "kos.h"
+
+/**
+ * The VDRIVER_PATH environment variable name.
+ *
+ * This environment variable may contain multiple paths separated by colons.
+ */
+#define VDRIVER_PATH_ENVVAR "VDRIVER_PATH"
+
+/**
+ * The default VDRIVER path.
+ *
+ * This is the path where the VDEV drivers are stored, relative to the library prefix.
+ * TODO This isn't always set, right? I gotta figure this sheiße out.
+ * It is used if the VDRIVER_PATH environment variable is not set.
+ */
+#define DEFAULT_VDRIVER_PATH "/vdriver"
+
+/**
+ * The file extension of VDRIVER files.
+ *
+ * Files with other extensions should be ignored.
+ */
+#define VDRIVER_EXT ".vdriver"
+
+/**
+ * Initialize VDRIVER loader global state.
+ *
+ * Call this before interacting with the loader.
+ */
+void vdriver_loader_init(void);
+
+/**
+ * Request all the VDEVs of a specific spec.
+ *
+ * This will look for and load the VDRIVER implementing this spec if it exists, 
+ *
+ * @param spec The specification of the VDEV to request.
+ * @param notif_cb The callback to call for {@link KOS_NOTIF_ATTACH} notifications.
+ * @param notif_data The data to pass to the notification callback.
+ * @return 0 on success, or a negative error code on failure.
+ */
+int vdriver_loader_req_vdev(char const* spec, kos_notif_cb_t notif_cb, void* notif_data);
+
+/**
+ * Take inventory of all VDEVs available on the system.
+ *
+ * This will load all VDRIVERs found and probe for all their VDEVs.
+ *
+ * @param notif_cb The callback to call for {@link KOS_NOTIF_ATTACH} notifications.
+ * @param notif_data The data to pass to the notification callback.
+ * @return 0 on success, or a negative error code on failure.
+ */
+int vdriver_loader_vdev_inventory(kos_notif_cb_t notif_cb, void* notif_data);
