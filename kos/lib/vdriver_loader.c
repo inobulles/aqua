520bc192a4b6609cbb6fc0d73b958df5ae1096ac
Aymeric Wibo <obiwac@gmail.com>
2025-08-07 20:14:04 +0200

    kos/vdriver_loader: Implement requesting VDEVs locally

--- kos/lib/vdriver_loader.c
+++ kos/lib/vdriver_loader.c
@@ -2,14 +2,16 @@
 // v. 1.0. Copyright (c) 2025 Aymeric Wibo
 
 #include "vdriver_loader.h"
-#include "vdev.h"
 
 #include <umber.h>
 
 #include <assert.h>
 #include <inttypes.h>
 #include <stdint.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
 #include <dlfcn.h>
 
@@ -28,13 +30,13 @@ void vdriver_loader_init(void) {
 	assert(cur_vid_slice == 0);
 }
 
-static int load_from_path(char const* path, kos_notif_cb_t notif_cb, void* notif_data) {
-	LOG_V(cls, "Loading VDRIVER from path: %s", path);
+static vdriver_t* load_from_path(char const* path, kos_notif_cb_t notif_cb, void* notif_data) {
+	LOG_V(cls, "Trying to load VDRIVER from path: %s", path);
 	void* const lib = dlopen(path, RTLD_LAZY);
 
 	if (lib == NULL) {
 		LOG_E(cls, "Failed to load VDRIVER: dlopen(\"%s\"): %s", path, dlerror());
-		return -1;
+		return NULL;
 	}
 
 	dlerror(); // Clear last error message.
@@ -45,7 +47,7 @@ static int load_from_path(char const* path, kos_notif_cb_t notif_cb, void* notif
 	if (vdriver == NULL) {
 		LOG_E(cls, "Failed to load VDRIVER 'VDRIVER' symbol: %s", dlerror());
 		dlclose(lib);
-		return -1;
+		return NULL;
 	}
 
 	LOG_V(cls, "Allocating VID slice for VDRIVER.", path);
@@ -70,22 +72,90 @@ static int load_from_path(char const* path, kos_notif_cb_t notif_cb, void* notif
 
 	LOG_I(cls, "VDRIVER loaded from '%s' (VID slice [0x%" PRIx64 ", 0x%" PRIx64 "]).", path, vdriver->vdev_id_lo, vdriver->vdev_id_hi);
 
-	return 0;
+	// Store the VDRIVER in the global array.
+
+	vdrivers = realloc(vdrivers, (vdriver_count + 1) * sizeof *vdrivers);
+	assert(vdrivers != NULL);
+	vdrivers[vdriver_count++] = vdriver;
+
+	return vdriver;
 }
 
-int vdriver_loader_req_vdev(char const* spec, kos_notif_cb_t notif_cb, void* notif_data) {
-	(void) spec;
-	(void) notif_cb;
-	(void) notif_data;
-	(void) load_from_path;
-
-	return 0;
+static void strfree(char** str) {
+	if (str != NULL) {
+		free(*str);
+	}
 }
 
-int vdriver_loader_vdev_inventory(kos_notif_cb_t notif_cb, void* notif_data) {
+void vdriver_loader_req_local_vdev(char const* spec, kos_notif_cb_t notif_cb, void* notif_data) {
+	LOG_V(cls, "Trying to find local VDRIVER providing spec \"%s\".", spec);
+
+	char* __attribute__((cleanup(strfree))) vdriver_path_tmp = NULL;
+	char* vdriver_path = getenv(VDRIVER_PATH_ENVVAR);
+
+	if (vdriver_path == NULL) {
+		char* const prefix = getenv(
+#if defined(__APPLE__)
+			"DY" // dyld(1) doesn't recognize `LD_LIBRARY_PATH`.
+#endif
+			"LD_LIBRARY_PATH"
+		);
+
+		if (prefix == NULL) {
+			vdriver_path = "/usr/local/lib/" DEFAULT_VDRIVER_PATH;
+		}
+
+		else {
+			asprintf(&vdriver_path_tmp, "%s/%s:/usr/local/lib/%s", prefix, DEFAULT_VDRIVER_PATH, DEFAULT_VDRIVER_PATH);
+			assert(vdriver_path_tmp != NULL);
+			vdriver_path = vdriver_path_tmp;
+		}
+	}
+
+	LOG_V(cls, "VDRIVER path is '%s' - going through each path and trying to load VDRIVER.", vdriver_path);
+
+	char* const path_copy_orig = strdup(vdriver_path);
+	char* path_copy = path_copy_orig;
+	char* tok;
+
+	while ((tok = strsep(&path_copy, ":")) != NULL) {
+		char* __attribute__((cleanup(strfree))) candidate = NULL;
+		asprintf(&candidate, "%s/%s.vdriver", tok, spec);
+		assert(candidate != NULL);
+
+		if (access(candidate, F_OK) != 0) {
+			continue;
+		}
+
+		// Driver file exists, we should be able to load it.
+
+		vdriver_t* const vdriver = load_from_path(candidate, notif_cb, notif_data);
+
+		if (vdriver == NULL) {
+			continue;
+		}
+
+		// Probe for VDEVs on that driver.
+
+		if (vdriver->probe == NULL) {
+			LOG_E(cls, "VDRIVER '%s' doesn't implement a probe function.", spec);
+			continue;
+		}
+
+		LOG_V(cls, "Probing VDRIVER for '%s' VDEVs.", spec);
+		vdriver->probe();
+	}
+
+	free(path_copy_orig);
+}
+
+int vdriver_loader_vdev_local_inventory(kos_notif_cb_t notif_cb, void* notif_data) {
+	LOG_V(cls, "Taking inventory of all VDEVs available on the system.");
+
+	// TODO
+
 	(void) notif_cb;
 	(void) notif_data;
-	(void) load_from_path;
 
 	return 0;
 }
