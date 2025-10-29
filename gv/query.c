4b3af929d02700ccf3d4950b9225071a0b0218ad
Aymeric Wibo <obiwac@gmail.com>
2025-10-29 11:45:21 +0100

    gv/query: Better logging

--- gv/query.c
+++ gv/query.c
@@ -7,7 +7,6 @@
 
 #include <assert.h>
 #include <errno.h>
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
@@ -18,17 +17,17 @@ static void close_sock(int const* sock) {
 	}
 }
 
-int query(in_addr_t in_addr, size_t* vdev_count_ref, kos_vdev_descr_t** vdevs_ref) {
-	// Create a new socket for sending to this node.
+int query(state_t* s, in_addr_t in_addr, size_t* vdev_count_ref, kos_vdev_descr_t** vdevs_ref) {
+	LOG_V(s->query_cls, "Create a new socket for sending to this node.");
 
 	int const __attribute__((cleanup(close_sock))) sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 
 	if (sock < 0) {
-		fprintf(stderr, "socket: %s\n", strerror(errno));
+		LOG_E(s->query_cls, "socket: %s", strerror(errno));
 		return -1;
 	}
 
-	// Connect to the node.
+	LOG_V(s->query_cls, "Connect to the node.");
 
 	struct sockaddr_in addr = {
 		.sin_family = AF_INET,
@@ -37,36 +36,36 @@ int query(in_addr_t in_addr, size_t* vdev_count_ref, kos_vdev_descr_t** vdevs_re
 	};
 
 	if (connect(sock, (struct sockaddr*) &addr, sizeof addr) < 0) {
-		fprintf(stderr, "connect: %s\n", strerror(errno));
+		LOG_E(s->query_cls, "connect: %s", strerror(errno));
 		return -1;
 	}
 
-	// Send QUERY packet.
+	LOG_V(s->query_cls, "Send QUERY packet.");
 
 	gv_packet_t packet = {
 		.header.type = GV_PACKET_TYPE_QUERY,
 	};
 
 	if (sendto(sock, &packet, sizeof packet.header, 0, (struct sockaddr*) &addr, sizeof addr) < 0) {
-		fprintf(stderr, "sendto: %s\n", strerror(errno));
+		LOG_E(s->query_cls, "sendto: %s", strerror(errno));
 		return -1;
 	}
 
-	// Receive QUERY_RES header.
+	LOG_V(s->query_cls, "Receive QUERY_RES header.");
 
 	size_t const query_res_size = sizeof packet.header + sizeof packet.query_res;
 
 	if (recv(sock, &packet, query_res_size, 0) != (ssize_t) query_res_size) {
-		fprintf(stderr, "recv: %s\n", strerror(errno));
+		LOG_E(s->query_cls, "recv: %s", strerror(errno));
 		return -1;
 	}
 
 	if (packet.header.type != GV_PACKET_TYPE_QUERY_RES) {
-		fprintf(stderr, "Unexpected packet type for QUERY response: %d\n", packet.header.type);
+		LOG_E(s->query_cls, "Unexpected packet type for QUERY response: %d", packet.header.type);
 		return -1;
 	}
 
-	// Receive VDEV descriptors themselves.
+	LOG_V(s->query_cls, "Receive VDEV descriptors themselves.");
 
 	size_t const vdev_count = packet.query_res.vdev_count;
 	size_t const vdev_bytes = vdev_count * sizeof(kos_vdev_descr_t);
@@ -75,10 +74,12 @@ int query(in_addr_t in_addr, size_t* vdev_count_ref, kos_vdev_descr_t** vdevs_re
 	assert(vdevs != NULL);
 
 	if (recv(sock, vdevs, vdev_bytes, 0) != (ssize_t) vdev_bytes) {
-		fprintf(stderr, "recv: %s\n", strerror(errno));
+		LOG_E(s->query_cls, "recv: %s", strerror(errno));
 		free(vdevs);
 		return -1;
 	}
+
+	LOG_V(s->query_cls, "Queried.");
 
 	*vdev_count_ref = vdev_count;
 	*vdevs_ref = vdevs;
@@ -100,10 +101,13 @@ int query_res(conn_t* conn) {
 	packet->query_res.vdev_count = state->vdev_count;
 	memcpy(packet->query_res.vdevs, state->vdevs, vdevs_size);
 
+	LOG_V(state->query_cls, "Sending QUERY_RES packet.");
+
 	if (send(conn->sock, packet, packet_size, 0) != (ssize_t) packet_size) {
-		fprintf(stderr, "sendto: %s\n", strerror(errno));
+		LOG_E(state->query_cls, "sendto: %s", strerror(errno));
 		return -1;
 	}
 
+	LOG_V(state->query_cls, "QUERY_RES packet sent.");
 	return 0;
 }
