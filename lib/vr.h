808af79093114655645f679bcad0eb3d5468e26c
Aymeric Wibo <obiwac@gmail.com>
2025-10-29 11:42:28 +0100

    lib: Library for VR VDEV

---
+++ lib/vr.h
@@ -0,0 +1,39 @@
+// This Source Form is subject to the terms of the AQUA Software License, v. 1.0.
+// Copyright (c) 2025 Aymeric Wibo
+
+#pragma once
+
+#include "root.h"
+
+/**
+ * VR library component context.
+ */
+typedef struct vr_ctx_t* vr_ctx_t;
+
+/**
+ * Initialize the VR library component.
+ *
+ * @param ctx The AQUA library context.
+ * @return The VR library component handle.
+ */
+aqua_component_t vr_init(aqua_ctx_t ctx);
+
+/**
+ * Connect to a VR VDEV.
+ *
+ * @param vdev The descriptor of the VR VDEV to connect to.
+ * @return The VR library component context or `NULL` if allocation failed.
+ */
+vr_ctx_t vr_conn(kos_vdev_descr_t const* vdev);
+
+/**
+ * Disconnect from a VR VDEV.
+ *
+ * @param ctx The VR library component context.
+ */
+void vr_disconn(vr_ctx_t ctx);
+
+/**
+ * TODO
+ */
+void vr_send_win(vr_ctx_t ctx, uint32_t id, uint32_t x_res, uint32_t y_res, void* fb);
