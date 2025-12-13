3a11c6064cf1e2aa6f16ed99dbc477aff80fa69a
Aymeric Wibo <obiwac@gmail.com>
2025-12-13 01:41:57 +0100

    vdev/ui/wgpu: Regular pipeline

---
+++ vdev/ui/pipeline_regular.go
@@ -0,0 +1,75 @@
+// This Source Form is subject to the terms of the AQUA Software License, v. 1.0.
+// Copyright (c) 2025 Aymeric Wibo
+
+package main
+
+import (
+	_ "embed"
+	"unsafe"
+
+	"obiw.ac/aqua/wgpu"
+)
+
+type RegularPipeline struct {
+	Pipeline
+	vbo_layout wgpu.VertexBufferLayout
+}
+
+//go:embed shaders/regular.wgsl
+var regular_shader_src string
+
+func (b *WgpuBackend) NewRegularPipeline() (*RegularPipeline, error) {
+	vbo_layout := wgpu.VertexBufferLayout{
+		ArrayStride: uint64(unsafe.Sizeof(Vertex{})),
+		StepMode:    wgpu.VertexStepModeVertex,
+		Attributes: []wgpu.VertexAttribute{
+			{ // Vertex position (x, y).
+				Format:         wgpu.VertexFormatFloat32x2,
+				Offset:         0,
+				ShaderLocation: 0,
+			},
+			{ // Texture coordinate (u, v).
+				Format:         wgpu.VertexFormatFloat32x2,
+				Offset:         4 * 2,
+				ShaderLocation: 1,
+			},
+		},
+	}
+
+	pipeline, err := b.NewPipeline("Regular", regular_shader_src,
+		[]wgpu.BindGroupLayoutEntry{
+			{ // Texture.
+				Binding:    0,
+				Visibility: wgpu.ShaderStageFragment,
+				Texture: wgpu.TextureBindingLayout{
+					Multisampled:  false,
+					ViewDimension: wgpu.TextureViewDimension2D,
+					SampleType:    wgpu.TextureSampleTypeFloat,
+				},
+			},
+			{ // Sampler.
+				Binding:    1,
+				Visibility: wgpu.ShaderStageFragment,
+				Sampler: wgpu.SamplerBindingLayout{
+					Type: wgpu.SamplerBindingTypeFiltering,
+				},
+			},
+		},
+		[]wgpu.VertexBufferLayout{
+			vbo_layout,
+		},
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &RegularPipeline{
+		Pipeline:   *pipeline,
+		vbo_layout: vbo_layout,
+	}, nil
+}
+
+func (pipeline *RegularPipeline) Release() {
+	pipeline.Pipeline.Release()
+}
