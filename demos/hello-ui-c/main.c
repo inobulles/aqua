2dcdcf346d64ce7d358518fcf8804c709aae2f21
Aymeric Wibo <obiwac@gmail.com>
2025-12-16 20:52:04 +0100

    demos/hello-ui-c: Add background colour to root div

--- demos/hello-ui-c/main.c
+++ demos/hello-ui-c/main.c
@@ -121,6 +121,12 @@ int main(void) {
 	}
 
 	ui_elem_t const root = ui_get_root(ui);
+
+	ui_set_attr(root, "bg.r", 1.0);
+	ui_set_attr(root, "bg.g", 0.0);
+	ui_set_attr(root, "bg.b", 1.0);
+	ui_set_attr(root, "bg.a", 0.5);
+
 	ui_add_text(root, "text.title", "Hello world!");
 	para = ui_add_text(root, "text.paragraph", "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.");
 
