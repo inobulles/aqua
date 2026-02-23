47589a61092ac0d3ff1f13b9298e1570ec9321e4
Aymeric Wibo <obiwac@gmail.com>
2026-02-23 20:06:20 +0100

    vdev/ui: Make abs calculation happen at render, so we can change absolute positions without having to reflow

--- vdev/ui/reflow.go
+++ vdev/ui/reflow.go
@@ -34,14 +34,7 @@ func (e *Div) reflow(max_w, max_h uint32) {
 		child.reflow(max_w-pl-pr, max_h-pt-pb)
 
 		if child.ElemBase().is_abs {
-			abs := child.ElemBase().abs
-
-			child.ElemBase().flow_x = e.dimension_to_px_x(abs.x) -
-				uint32(abs.anchor_x*float32(child.ElemBase().flow_w))
-			child.ElemBase().flow_y = e.dimension_to_px_y(abs.y) -
-				uint32(abs.anchor_y*float32(child.ElemBase().flow_h))
-
-			continue
+			continue // Take element out of flow if absolutely positioned.
 		}
 
 		cw, ch := child.ElemBase().flow_w, child.ElemBase().flow_h
