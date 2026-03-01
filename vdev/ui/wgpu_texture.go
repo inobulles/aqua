ffd490bb0894f975e9bd6de879a064fb1f2f8420
Aymeric Wibo <obiwac@gmail.com>
2026-03-01 13:16:04 +0100

    vdev/ui: Start work on infrastructure required for multiple render passes/textures

--- vdev/ui/wgpu_texture.go
+++ vdev/ui/wgpu_texture.go
@@ -29,14 +29,19 @@ func (t *WgpuTexture) CreateSampler(b *WgpuBackend) error {
 	return nil
 }
 
-func (b *WgpuBackend) NewTexture(name string, w, h uint32, data []byte) (*WgpuTexture, error) {
-	tex := WgpuTexture{}
-
-	tex_size := wgpu.Extent3D{
+func create_tex_size(w, h uint32) wgpu.Extent3D {
+	return wgpu.Extent3D{
 		Width:              w,
 		Height:             h,
 		DepthOrArrayLayers: 1,
 	}
+}
+
+func create_texture_common(
+	b *WgpuBackend, name string, tex_size wgpu.Extent3D,
+	format wgpu.TextureFormat, usage wgpu.TextureUsage,
+) (*WgpuTexture, error) {
+	tex := WgpuTexture{}
 
 	var err error
 
@@ -46,9 +51,33 @@ func (b *WgpuBackend) NewTexture(name string, w, h uint32, data []byte) (*WgpuTe
 		MipLevelCount: 1,
 		SampleCount:   1,
 		Dimension:     wgpu.TextureDimension2D,
-		Format:        wgpu.TextureFormatRGBA8Unorm,
-		Usage:         wgpu.TextureUsageTextureBinding | wgpu.TextureUsageCopyDst,
+		Format:        format,
+		Usage:         usage,
 	}); err != nil {
+		return nil, err
+	}
+
+	if tex.view, err = tex.tex.CreateView(nil); err != nil {
+		tex.tex.Release()
+		return nil, err
+	}
+
+	if err = tex.CreateSampler(b); err != nil {
+		tex.tex.Release()
+		tex.view.Release()
+		return nil, err
+	}
+
+	return &tex, nil
+}
+
+func (b *WgpuBackend) NewTexture(name string, w, h uint32, data []byte) (*WgpuTexture, error) {
+	tex_size := create_tex_size(w, h)
+	tex, err := create_texture_common(b, name, tex_size,
+		wgpu.TextureFormatRGBA8Unorm,
+		wgpu.TextureUsageTextureBinding|wgpu.TextureUsageCopyDst)
+
+	if err != nil {
 		return nil, err
 	}
 
@@ -66,18 +95,12 @@ func (b *WgpuBackend) NewTexture(name string, w, h uint32, data []byte) (*WgpuTe
 		return nil, err
 	}
 
-	if tex.view, err = tex.tex.CreateView(nil); err != nil {
-		tex.tex.Release()
-		return nil, err
-	}
+	return tex, nil
+}
 
-	if err = tex.CreateSampler(b); err != nil {
-		tex.tex.Release()
-		tex.view.Release()
-		return nil, err
-	}
-
-	return &tex, nil
+func (b *WgpuBackend) NewRenderBuf(name string, w, h uint32, format wgpu.TextureFormat) (*WgpuTexture, error) {
+	return create_texture_common(b, name, create_tex_size(w, h), format,
+		wgpu.TextureUsageCopySrc|wgpu.TextureUsageRenderAttachment|wgpu.TextureUsageCopyDst)
 }
 
 func (t *WgpuTexture) Release() {
